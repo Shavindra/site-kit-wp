8c5c92b99fb426d156e68272b93f0d4c27bfb367
shavindra <shavindra@users.noreply.github.com>
2020-02-18 17:16:38 +0100

    fixed tests

--- tests/e2e/specs/dashboard/dashboard-noscript.test.js
+++ tests/e2e/specs/dashboard/dashboard-noscript.test.js
@@ -1,48 +1,35 @@
 /**
  * WordPress dependencies
  */
-import { activatePlugin, visitAdminPage } from '@wordpress/e2e-test-utils';
-
-/**
- * Internal dependencies
- */
-import { setSiteVerification, resetSiteKit } from '../../utils';
-
-const activateSiteKit = async () => {
-	try {
-		await activatePlugin( 'google-site-kit' );
-	} catch {
-		await activatePlugin( 'site-kit-by-google' );
-	}
-};
-
-const deactivateSiteKit = async () => {
-	try {
-		await deactivatePlugin( 'google-site-kit' );
-	} catch {
-		await deactivatePlugin( 'site-kit-by-google' );
-	}
-};
+import { visitAdminPage } from '@wordpress/e2e-test-utils';
 
 describe( 'Site Kit noscript message', () => {
-	beforeAll( async () => {
-		page.setJavaScriptEnabled( false );
-		await activateSiteKit();
-		// await setSiteVerification();
-	} );
 
 	beforeEach( async () => {
 		await visitAdminPage( 'admin.php', 'page=googlesitekit-splash' );
 	} );
 
+	describe('When Javascript is enabled', () => {
+		it('Should not display noscript message when javascript is ', async () => {
+			const noscript = await page.$( '#wpbody-content' );
+			await expect(noscript).not.toMatchElement('.googlesitekit-noscript__title')
+		});
+	})
 
-	afterEach( async () => {
-		await deactivateSiteKit();
-		await resetSiteKit();
-	} );
-
-	it( 'Should display noscript message', async () => {
-		 await page.$( '.googlesitekit-noscript' );
-		await expect( noJS ).toMatchElement( '.googlesitekit-googlesitekit-noscript__title', { text: 'HThe Site Kit by Google plugin requires JavaScript to be enabled in your browser.' } );
-	} );
+	describe('When Javascript is disabled', () => {
+		beforeAll( async () => {
+			page.setJavaScriptEnabled( false );
+		} );
+	
+		afterAll( async () => {
+			page.setJavaScriptEnabled( true );
+		} );
+	
+		it( 'Should display noscript message when javascript is ', async () => {
+			const noscript = await page.waitForSelector('.googlesitekit-noscript', {
+				visible: true,
+			  })
+			await expect( noscript ).toMatchElement( '.googlesitekit-noscript__title', { text: 'The Site Kit by Google plugin requires JavaScript to be enabled in your browser.' } );
+		} );
+	})
 } );
